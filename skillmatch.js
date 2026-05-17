1a1e3a695686531051609a48111d953cc7d05d3c
andrea7070mierzwa <andrea7070mierzwa@gmail.com>
2026-05-17 05:29:18 -0300

    Criação da clasee SoftSkills

---
+++ skillmatch.js
@@ -0,0 +1,30 @@
+ class SoftSkills {
+  constructor() {
+    this.softSkillsSenai = [
+      "Trabalho em equipe",
+      "Comunicação",
+      "Proatividade",
+      "Responsabilidade",
+      "Organização",
+      "Pensamento crítico",
+      "Criatividade",
+      "Resolução de problemas",
+      "Adaptabilidade",
+      "Liderança",
+      "Inteligência emocional",
+      "Ética profissional",
+      "Comprometimento",
+      "Aprendizagem contínua",
+      "Gestão do tempo",
+      "Colaboração",
+      "Empatia",
+      "Autonomia",
+      "Disciplina",
+      "Inovação"
+    ]
+  }
+}
+const skills = new SoftSkills()
+
+console.log(skills.softSkillsSenai)
+
